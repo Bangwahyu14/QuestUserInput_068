902b5cc0dffe0c8f31790e4046dc1b29fcd8a1ea
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-30 12:02:43 +0700

    membuat column modifier

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,11 +1,14 @@
 package com.example.userinput
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -20,4 +23,7 @@ fun FormDataDiri(modifier: Modifier
 
     val gender:List<String> = listOf("laki-laki","perempuan")
 
+    Column(modifier = Modifier.padding(top =50.dp),
+
+    )
 }
