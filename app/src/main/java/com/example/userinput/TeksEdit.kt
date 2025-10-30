e736ccfbedecafc34fb10c137a1fff61c0e4ee0c
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-30 11:51:57 +0700

    menambahkan var textnama,textalamat,texttjk

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,10 +1,17 @@
 package com.example.userinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
+    var texsNama by remember { mutableStateOf(value = "") }
+    var texsAlamat by remember { mutableStateOf(value = "") }
+    var texstJK by remember {mutableStateOf("")}
 
 }
