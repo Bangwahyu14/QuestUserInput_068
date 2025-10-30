f6565ecc35d46589c4686c949cb3b1554d0a53c9
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-30 12:36:58 +0700

    menambahkan  column di eleventedcard

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -32,20 +32,22 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
-){
+) {
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember { mutableStateOf("") }
-    var textJK by remember {mutableStateOf("")}
+    var textJK by remember { mutableStateOf("") }
 
-    var nama by remember {mutableStateOf("")}
-    var alamat by remember {mutableStateOf("")}
-    var jenis by remember {mutableStateOf("")}
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 
-    val gender:List<String> = listOf("laki-laki","perempuan")
+    val gender: List<String> = listOf("laki-laki", "perempuan")
 
-    Column(modifier = Modifier.padding(top =50.dp),
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
             value = textNama,
             singleLine = true,
@@ -55,13 +57,14 @@ fun FormDataDiri(modifier: Modifier
             onValueChange = {
                 textNama = it
             }
-    )
+        )
         Row {
             gender.forEach { item ->
-                Row(modifier = Modifier.selectable(
-                    selected = textJK == item,
-                    onClick = {textJK = item}
-                ), verticalAlignment = Alignment.CenterVertically) {
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = textJK == item,
+                        onClick = { textJK = item }
+                    ), verticalAlignment = Alignment.CenterVertically) {
                     RadioButton(
                         selected = textJK == item,
                         onClick = {
@@ -81,9 +84,11 @@ fun FormDataDiri(modifier: Modifier
             }
         )
         Divider(
-            modifier.padding(bottom = dimensionResource(R.dimen.padding_medium),top=dimensionResource(
-                R.dimen.padding_medium
-            )),
+            modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                    R.dimen.padding_medium
+                )
+            ),
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
@@ -91,17 +96,19 @@ fun FormDataDiri(modifier: Modifier
             modifier = Modifier.fillMaxWidth(1f),
             enabled = textAlamat.isNotEmpty(),
             onClick = {
-                nama=textNama
-                jenis=textJK
-                alamat=textAlamat
+                nama = textNama
+                jenis = textJK
+                alamat = textAlamat
             }
         ) {
             Text(stringResource(R.string.submit))
         }
         Divider(
-            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
-                id = R.dimen.padding_medium
-            )),
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )
+            ),
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
@@ -111,6 +118,13 @@ fun FormDataDiri(modifier: Modifier
             modifier = Modifier
                 .height(100.dp)
                 .width(300.dp)
-        ){}
+        ) {
+            Column(modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),) {
+                Text(text = "Nama   :" + nama, color = Color.White)
+                Text(text = "Gender :" + jenis, color = Color.White)
+                Text(text = "Alamat :" + alamat, color = Color.White)
+            }
+        }
     }
 }
+
